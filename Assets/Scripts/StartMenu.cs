40b207d74df470c4e1385a358e0fe9bf55885f14
spacebit <swpett10@gmail.com>
2021-01-21 16:10:20 -0500

    main menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartMenu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+}
